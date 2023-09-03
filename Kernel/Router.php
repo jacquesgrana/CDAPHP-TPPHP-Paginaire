541a601c9d7f3225a47d11ba05259850a2eeb495
jacques <jac.granarolo.info@gmail.com>
2023-09-03 15:13:33 +0200

    amélioration du routeur : (re)mise en place de la vérification de l'existence du contrôleur et de la méthode avant les affectations

--- Kernel/Router.php
+++ Kernel/Router.php
@@ -1,45 +1,49 @@
 <?php
- /**
-  * Classe du routeur qui selon la querystring appelle le contrôleur
-  * correspondant et appelle (ou non) la méthode passée (ou non) 
-  * en paramètre.
-  */
-class Router {
+
+/**
+ * Classe du routeur qui selon la querystring appelle le contrôleur
+ * correspondant et appelle (ou non) la méthode passée (ou non) 
+ * en paramètre.
+ */
+class Router
+{
     private $controller;
     private $method;
 
+    /**
+     * Constructeur : vérifie l'existence du contrôleur 
+     * et de la méthode demandés dans la querystring et 
+     * affecte le contrôleur et la méthode.
+     */
     public function __construct()
     {
         $this->controller = $_SERVER["DOCUMENT_ROOT"] . '/Controller/Index.php';
         $this->method = 'index';
-
-        if (isset($_GET['page'])) {  
-
-            $this->controller = $_SERVER["DOCUMENT_ROOT"] . '/Controller/'.$_GET['page'].'.php';
-            /*
-            if(class_exists($_SERVER["DOCUMENT_ROOT"] . '/Controller/'.$_GET['page'])) {
-                $this->controller = $_SERVER["DOCUMENT_ROOT"] . '/Controller/'.$_GET['page'].'.php';
-            }*/
-            
+        $isPageOk = false;
+        if (isset($_GET['page'])) {
+            $controllerFile = $_SERVER["DOCUMENT_ROOT"] . '/Controller/' . $_GET['page'] . '.php';
+            if (file_exists($controllerFile)) {
+                $this->controller = $controllerFile;
+                include_once $this->controller;
+                $isPageOk = true;
+            }
         }
 
-        if (isset($_GET['method'])) {
-            $this->method = $_GET['method'];
-            /*
-            if (method_exists($this->controller, $_GET['method'])) {
-                $this->method = $_GET['method'];
-            } 
-            else {
-                $this->controller = $_SERVER["DOCUMENT_ROOT"] . '/Controller/Index.php';
-            }   */ 
+        if (isset($_GET['method']) && $isPageOk) {
+            $className = basename($this->controller, '.php');
+            $object = new $className();
+            $methodName = $_GET['method'];
+            if (method_exists($object, $methodName)) {
+                $this->method = $methodName;
+            }
         }
     }
 
     /**
      * Fonction qui instancie le contrôleur et appelle la méthode.
      */
-    public function doRoute() {
-        include_once $this->controller;
+    public function doRoute()
+    {
         $method = $this->method;
         $className = basename($this->controller, '.php');
         $controller = new $className;
